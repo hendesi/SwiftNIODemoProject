200c63825dd66ed139c5824c600f44a33acc1b3a
Felix Desiderato <felix.desiderato@tum.de>
2021-06-22 19:13:11 +0200

    Try develop branch of ATRK

--- Package.swift
+++ Package.swift
@@ -12,7 +12,8 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.18.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.8.0"),
         .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.13.0"),
-        .package(url: "https://github.com/nerdsupremacist/AssociatedTypeRequirementsKit.git", .upToNextMinor(from: "0.3.2")),
+        .package(url: "https://github.com/nerdsupremacist/AssociatedTypeRequirementsKit.git", .branch("develop"))
+//        .package(url: "https://github.com/nerdsupremacist/AssociatedTypeRequirementsKit.git", .upToNextMinor(from: "0.3.2")),
     ],
     targets: [
         .target(
