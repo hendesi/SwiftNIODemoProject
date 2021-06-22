85a61bdf188cbcc7050255a4db007e1ffaca76b2
Felix Desiderato <felix.desiderato@tum.de>
2021-06-22 14:14:48 +0200

    Added AssociatedTypeRequirementsKit

--- Package.swift
+++ Package.swift
@@ -5,10 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "Test",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.18.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.8.0"),
-        .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.13.0")
+        .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.13.0"),
+        .package(url: "https://github.com/nerdsupremacist/AssociatedTypeRequirementsKit.git", .upToNextMinor(from: "0.3.2")),
     ],
     targets: [
         .target(
@@ -17,6 +21,7 @@ let package = Package(
                 .product(name: "NIO", package: "swift-nio"),
                 .product(name: "NIOHTTP2", package: "swift-nio-http2"),
                 .product(name: "NIOSSL", package: "swift-nio-ssl"),
+                .product(name: "AssociatedTypeRequirementsKit", package: "AssociatedTypeRequirementsKit")
             ]
         ),
         .testTarget(
--- Sources/Test/main.swift
+++ Sources/Test/main.swift
@@ -1,1 +1,21 @@
-print("Hello, world!")
+import AssociatedTypeRequirementsVisitor
+import SwiftUI
+
+private let converter = AnyViewConverter()
+
+extension AnyView {
+    init?(_ value: Any) {
+        guard let view = converter(value) else { return nil }
+        self = view
+    }
+}
+
+private struct AnyViewConverter : ViewVisitor {
+    // Provide a function that can be called with all the necessary type information
+    func callAsFunction<T : View>(_ value: T) -> AnyView {
+        print(#function)
+        return AnyView(value)
+    }
+}
+
+let _ = converter.callAsFunction(12)
